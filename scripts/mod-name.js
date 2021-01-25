0c9839c40108ec921c783bf1221f07a581a256da
1qwaka <70287659+1qwaka@users.noreply.github.com>
2021-01-25 11:13:08 +0500

    Add files via upload

---
+++ scripts/mod-name.js
@@ -0,0 +1,3 @@
+Vars.mods.locateMod("item-transfer").meta.displayName = Core.bundle.get("mod.item-transfer.displayname");
+Vars.mods.locateMod("item-transfer").meta.description = Core.bundle.get("mod.item-transfer.description");
+Vars.mods.locateMod("item-transfer").meta.author = Core.bundle.get("mod.item-transfer.author");
